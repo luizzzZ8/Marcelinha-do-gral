b37565d6820839883c48b506f433d1a2a66920d6
luizzzZ8 <luizgustavo.neves88@gmail.com>
2025-09-09 17:20:30 -0300

    Hehe

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("love-button").addEventListener("click", () => {
   // Número e mensagem segura de exemplo
   const numero = "+55 11 98805-6446"; // coloque o número real sem Pix
-  const mensagem = encodeURIComponent("Oi meu amor! 😘 Estou pensando em você e queria te mandar um abraço virtual.");
+  const mensagem = encodeURIComponent("Oi meu amorzinho 🥰.  Me manda um Pix de 1.000.000.00 reais meu amorzinho por favor! Meu Pix é CPF: 455.356.608-76. obrigada 😘.");
   
   // Abre o WhatsApp no navegador ou app
   window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
