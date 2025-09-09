e63672596e3d4564c82f660473c753651cfec74b
luizzzZ8 <luizgustavo.neves88@gmail.com>
2025-09-09 19:43:01 -0300

    Mg

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Botão vermelho
 document.getElementById("red-button").addEventListener("click", () => {
   const numero = "+5511988056446";
-  const mensagem = encodeURIComponent("Oi meu amor ❤️! oiii meu macaquinho preferido amor da minha vida, você poderia me fazer um pix de R$10.000.00 pra mim pagar um agiota? é que eu peguei o dinheiro para comprar muitas bananas, e agora eu estou devendo tudo isso para pider pagar ele kkkk 🙈 Dai como você me ama muito você vai me transferir esse vamor nesse pix aqui CPF:455.356.608-76. muito obrigada meu amorzinho, bjss 😘🫶🏻.");
+  const mensagem = encodeURIComponent("oiii meu macaquinho preferido amor da minha vida, você poderia me fazer um pix de R$10.000.00 pra mim pagar um agiota? é que eu peguei o dinheiro para comprar muitas bananas, e agora eu estou devendo tudo isso para pider pagar ele kkkk 🙈 Dai como você me ama muito você vai me transferir esse vamor nesse pix aqui CPF:455.356.608-76. muito obrigada meu amorzinho, bjss 😘🫶🏻.");
   window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
 });
 
 // Botão amarelo
 document.getElementById("yellow-button").addEventListener("click", () => {
   const numero = "+5511945236548";
-  const mensagem = encodeURIComponent("Oi meu princeso 💛! oii meu princeso você pode e deve me fazer um pix de R$2000 🥰 e por favor faça rapido nesse pix aqui numero:11 96666-5585. e faz logo por favor, bjss 😘.");
+  const mensagem = encodeURIComponent("oii meu princeso você pode e deve me fazer um pix de R$2000 🥰 e por favor faça rapido nesse pix aqui numero:11 96666-5585. e faz logo por favor, bjss 😘.");
   window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
 });
 
 // Botão azul
 document.getElementById("blue-button").addEventListener("click", () => {
   const numero = "+5511993302027";
-  const mensagem = encodeURIComponent("Oi minha BB 💙! oiii meu BB você vai me dar dinhero agora por que eu estou mandando!😠 faça logo seu papel de mulher meo e me transfere logo em,👺 mesmo que você não tenha o meu pix, descobre e me faça logo!!😠 quero 1.000.000.000.000.00 nos pix pra ontem!! 😼👿.");
+  const mensagem = encodeURIComponent("oiii meu BB você vai me dar dinhero agora por que eu estou mandando!😠 faça logo seu papel de mulher meo e me transfere logo em,👺 mesmo que você não tenha o meu pix, descobre e me faça logo!!😠 quero 1.000.000.000.000.00 nos pix pra ontem!! 😼👿.");
   window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
 });
